c385294b8d24786909f54632fcf792e19d9b01a4
petitviolet <violethero0820@gmail.com>
2015-02-09 00:08:12 +0900

    zip code search controller

---
+++ zip_address/controllers/zip.py
@@ -0,0 +1,33 @@
+# -*- encoding:utf-8 -*-
+''' zip code controller
+'''
+from zip_address.models.addr import AdAddress
+from zip_address.response_format import convert_format
+import re
+zip_regex = re.compile(r'\d{3}-\d{2,4}')
+
+class Zip(object):
+    @classmethod
+    def search(cls, zip):
+        ''' zip code search
+        :param zip: given zip code
+        :returns json formatted SQL result
+        '''
+        is_valid = cls.validate_zip(zip)
+        if not is_valid:
+            return False
+        result = AdAddress.query.filter(AdAddress.zip == zip)
+        data = [r.to_dict() for r in result.all()] if result.count() else {}
+        return convert_format(data, False)
+
+    @classmethod
+    def validate_zip(cls, zip):
+        ''' zip code validator
+        :param zip: given zip code
+        :returns True or False
+        '''
+        if len(zip) != 8:
+            return False
+        if not zip_regex.findall(zip):
+            return False
+        return True
