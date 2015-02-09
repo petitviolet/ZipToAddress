38f08c83f1ebee42a262713453f2a88bca6af670
petitviolet <violethero0820@gmail.com>
2015-02-09 09:33:00 +0900

    modify / to returns test response

--- zip_address/apis.py
+++ zip_address/apis.py
@@ -4,17 +4,19 @@
 from logger import logger
 from flask import (Blueprint, request, copy_current_request_context,
                    Response, redirect, url_for)
-from urllib.parse import unquote
-from threading import Thread
 from config import project_name
 from zip_address.controllers.zip import Zip
 from zip_address.error_response import ErrorResponse
+import json
 
 blueprint = Blueprint(project_name, __name__)
 
 @blueprint.route('/', methods=['GET'])
 def index():
-    return "test response"
+    result = {'result': 'test response'}
+    response = Response(json.dumps(result), mimetype='application/json')
+    response.status_code = 200 if result else 204
+    return response
 
 @blueprint.route('/zip_search', methods=['GET'])
 def zip_search():
